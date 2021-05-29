6cfda86c59244ad3025a925511c6069270197cc5
Viktor K. Körner <koerner-vk@protonmail.com>
2021-05-30 00:50:12 +0200

    Basic Script

---
+++ main.py
@@ -0,0 +1,44 @@
+import mouse
+import time
+
+
+# mouse.click('right')
+
+# for _ in range(100):
+# 	time.sleep(0.2)
+# 	print(mouse.get_position())
+
+stepover = 130
+stepdown = 150
+
+def fill_line(i):
+	if i > 0:
+		mouse.click('left')
+	for i in range(7):
+		mouse.move(stepover, 0, absolute=False, duration=0.05)
+		mouse.click('left')
+
+def doit():
+	print('Stash it!')
+	x_old, y_old = mouse.get_position()
+	for i in range(4):
+		mouse.move(x_old, y_old + i * stepdown, absolute=True, duration=0.05)
+		fill_line(i)
+	mouse.move(x_old, y_old, absolute=True, duration=0.05)
+
+mouse.on_right_click(lambda: doit())
+
+#time.sleep(300)
+on = False
+
+def toggle():
+	global on
+	on = not on
+	mouse.unhook_all()
+	if on:
+		print('Toggling on')
+		mouse.on_right_click(lambda: doit())
+	else:
+		print('Toggling off')
+
+toggle()
